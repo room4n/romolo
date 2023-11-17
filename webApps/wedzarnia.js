614ab3e21053d98c7bcd5dad8372b1e7a676b45d
Jarek <room4n@gmail.com>
2023-11-17 18:19:16 +0100

    connection

--- webApps/wedzarnia.js
+++ webApps/wedzarnia.js
@@ -3,7 +3,7 @@ const wedzarniaRoutes = express.Router();
 const connection = require("../config/database");
 
 wedzarniaRoutes.get('/', (req,res,next)=>{
-    con.query("select * from Products",(err,result)=>{
+    connection.query("select * from Products",(err,result)=>{
         if(err){
             res.send(err.message)
         } else {
