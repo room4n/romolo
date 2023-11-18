6d735c5ad39e7553566e5d302d7f62d14465e638
Jarek <room4n@gmail.com>
2023-11-18 12:08:53 +0100

    test

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const server = express();
 
 const wedzarniaRoutes = require('./webApps/wedzarnia');
 
-server.use('/wedzarnia', express.static(path.join(__dirname + '/client/wedzarnia/build')));
+server.use('/', express.static(path.join(__dirname + '/client/wedzarnia/build')));
 server.use('/wedzarnia/api', wedzarniaRoutes);
 module.exports = server;
