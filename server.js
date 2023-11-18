4e744989db66bb8cbc12a66536e3023caa383aef
Jarek <room4n@gmail.com>
2023-11-18 12:06:13 +0100

    wedzarnia client react

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 const server = express();
+
 const wedzarniaRoutes = require('./webApps/wedzarnia');
 
-server.use('/wedzarnia', wedzarniaRoutes);
-
+server.use('/wedzarnia', express.static(path.join(__dirname + '/client/wedzarnia/build')));
+server.use('/wedzarnia/api', wedzarniaRoutes);
 module.exports = server;
