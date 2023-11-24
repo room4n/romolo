88c45729a56717f3d764e4dc239bf4cea8ac4e4b
Jarek <room4n@gmail.com>
2023-11-24 14:12:27 +0100

    date fix

--- webApps/wedzarnia.js
+++ webApps/wedzarnia.js
@@ -28,7 +28,7 @@ wedzarniaRoutes.post('/addEntry',jsonParser,(req,res,next)=>{
             
             res.status(200).json({
                 message: {
-                    last: result[0].date
+                    last: new Date(result[0].date).getDate()
                 } 
             })
         }
