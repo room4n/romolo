d2609a894e2899a87c1c2bf871aa4e97bc6768a7
Jarek <room4n@gmail.com>
2023-11-28 17:59:48 +0100

    structure

--- webApps/wedzarnia.js
+++ webApps/wedzarnia.js
@@ -22,22 +22,29 @@ wedzarniaRoutes.post('/addEntry',jsonParser,(req,res,next)=>{
         month:  '2-digit', 
         year:   'numeric',
         hour12: false 
- };
- var formatOptionsTime = { 
-    hour:   '2-digit', 
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false 
-};
+    };
+    var formatOptionsTime = { 
+        hour:   '2-digit', 
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false 
+    };
+    var smokeID;
+    var lastDate;
+    var currentDate;
+    var currentTime;
+
     connection.query("select id, date from SmokeDay ORDER BY id DESC LIMIT 1",(err,result)=>{
         if(err){
             res.send(err.message)
         } else {
-            var smokeID = result[0].id;
-            var lastDate = new Date(JSON.parse(JSON.stringify(result[0].date))).toLocaleDateString('pl-PL',formatOptionsDate);
-            var currentDate = new Date().toLocaleDateString('pl-PL',formatOptionsDate);
-            var currentTime = new Date().toLocaleDateString('pl-PL',formatOptionsTime).split(', ')[1];
-            
+            smokeID = result[0].id;
+            lastDate = new Date(JSON.parse(JSON.stringify(result[0].date))).toLocaleDateString('pl-PL',formatOptionsDate);
+            currentDate = new Date().toLocaleDateString('pl-PL',formatOptionsDate);
+            currentTime = new Date().toLocaleDateString('pl-PL',formatOptionsTime).split(', ')[1];
+        }});
+        connection.end();
+
          //if dates are same
             if(lastDate != currentDate){
                 //add entry with the same smoke id
@@ -49,7 +56,8 @@ wedzarniaRoutes.post('/addEntry',jsonParser,(req,res,next)=>{
                     } else {
                         status = true;
                     }
-                })
+                });
+                connection.end();
             } else {
                 //create new smoke id entry 
                 //add entry with new smoke id
@@ -62,11 +70,7 @@ wedzarniaRoutes.post('/addEntry',jsonParser,(req,res,next)=>{
                 bottomTemp: req.body.tempBottom,
                 status: status
             })
-        }
-    });
-   
-    
-});
+        });
 
 /* function insertEntry(entryID, time, tempBottom, tempTop, prod1Temp, prod2Temp){
     connection.query("insert into Entries (smokeID, dateTime, tempBottom, tempTop, product1Temp, product2Temp) values ("+entryID+","+timr+","+tempBottom+","+tempTop+","+prod1Temp+","+prod2Temp+")",(err,result)=>{
