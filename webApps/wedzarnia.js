de751ee38bd740c6586c3520f15958f46c310030
Jarek Romaniuk <jaroslaw.romaniuk@pl.abb.com>
2023-11-28 15:00:31 +0100

    clean

--- webApps/wedzarnia.js
+++ webApps/wedzarnia.js
@@ -17,24 +17,25 @@ wedzarniaRoutes.get('/', (req,res,next)=>{
     })
 });
 wedzarniaRoutes.post('/addEntry',jsonParser,(req,res,next)=>{
-    
+    var formatOptions = { 
+        day:    '2-digit', 
+        month:  '2-digit', 
+        year:   'numeric',
+        //hour:   '2-digit', 
+        //minute: '2-digit',
+        hour12: false 
+ };
     connection.query("select id, date from SmokeDay ORDER BY id DESC LIMIT 1",(err,result)=>{
         if(err){
             res.send(err.message)
         } else {
-            var lastDate = new Date(JSON.parse(JSON.stringify(result[0].date)));
             var smokeID = result[0].id;
-            var currentDate = new Date();
-            var formatOptions = { 
-                day:    '2-digit', 
-                month:  '2-digit', 
-                year:   'numeric',
-                //hour:   '2-digit', 
-                //minute: '2-digit',
-                hour12: false 
-         };
-         currentDate = currentDate.toLocaleDateString('pl-PL',formatOptions);
-         lastDate = lastDate.toLocaleDateString('pl-PL',formatOptions);
+            var lastDate = new Date(JSON.parse(JSON.stringify(result[0].date))).toLocaleDateString('pl-PL',formatOptions);
+            var currentDate = new Date().toLocaleDateString('pl-PL',formatOptions);
+            
+         //currentDate = currentDate.toLocaleDateString('pl-PL',formatOptions);
+         //lastDate = lastDate.toLocaleDateString('pl-PL',formatOptions);
+         
          //if dates are same
             if(lastDate == currentDate){
                 
@@ -48,9 +49,7 @@ wedzarniaRoutes.post('/addEntry',jsonParser,(req,res,next)=>{
             })
         }
     });
-    //compare todays date with last smoke day date
-    
-    //var isSameDay = (lastSmokeDate == currentDay);
+   
     
 })
 
