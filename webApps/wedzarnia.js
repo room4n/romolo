1aa4edbede1ec43c2ecd5b90f8d853c07b625931
Jarek Romaniuk <jaroslaw.romaniuk@pl.abb.com>
2023-11-29 11:25:56 +0100

    insert

--- webApps/wedzarnia.js
+++ webApps/wedzarnia.js
@@ -68,18 +68,7 @@ wedzarniaRoutes.get('/', (req,res,next)=>{
             //}
             
 //});
-async function getLastSmoke(){
-    await connection.query("select id, date from SmokeDay ORDER BY id DESC LIMIT 1", (err, result) => {
-        if (err) {
-            return err.message;
-        } else {
-            return {
-                smokeID: result[0].id,
-                lastDate: new Date(JSON.parse(JSON.stringify(result[0].date))).toLocaleDateString('pl-PL', formatOptionsDate)
-            };
-        }
-    });
-}
+
 
 wedzarniaRoutes.post('/addEntry',jsonParser, async (req,res,next)=>{
     var formatOptions = { 
@@ -91,23 +80,43 @@ wedzarniaRoutes.post('/addEntry',jsonParser, async (req,res,next)=>{
         second: '2-digit',
         hour12: false 
     };
-
-    var lastEntry = await getLastEntry();
-    var smokeID = lastEntry.id;
-    var lastDate = new Date(JSON.parse(JSON.stringify(lastEntry.date))).toLocaleDateString('pl-PL',formatOptions);
+    var status;
+    var lastEntry = await getLastSmokeDay();
+    var lastSmokeID = lastEntry.id;
+    var lastSmokeDate = new Date(JSON.parse(JSON.stringify(lastEntry.date))).toLocaleDateString('pl-PL',formatOptions).split(', ')[0];
     var currentDate = new Date().toLocaleDateString('pl-PL',formatOptions).split(', ')[0];
     var currentTime = new Date().toLocaleDateString('pl-PL',formatOptions).split(', ')[1];
 
+    if(lastDate == currentDate){
+        //insert into entries table new entry with last smokedayid
+    } else {
+        //create new smokeday entry
+        //inser into entries with new smokeday entry
+
+        //draft
+        //insertEntry(smokeID, currentTime ,req.body.tempBottom, req.body.tempTop,req.body.prod1Temp,req.body.prod2Temp);
+        //(smokeID, dateTime, tempBottom, tempTop, product1Temp, product2Temp)
+        var data = {
+            smokeID: lastSmokeID, 
+            dateTime: currentTime, 
+            tempBottom: req.body.tempBottmom, 
+            tempTop: req.body.tempTop, 
+            product1Temp: req.body.prod1Temp, 
+            product2Temp: req.body.prod2Temp
+        }
+        status = await insertEntry(data);
+    }
 
     res.status(200).json({
-        lastID: smokeID,
-        lastDate: lastDate,
+        lastID: lastSmokeID,
+        lastDate: lastSmokeDate,
         currentDate: currentDate,
-        currentTime: currentTime
+        currentTime: currentTime,
+        status: status
     })
 });
 
-async function getLastEntry(){
+async function getLastSmokeDay(){
     return new Promise((resolve, reject)=>{
         connection.query("select id, date from SmokeDay ORDER BY id DESC LIMIT 1",(err,result)=>{
             if(err){
@@ -118,6 +127,18 @@ async function getLastEntry(){
         })
     });
 };
+
+async function insertEntry(dataSet){
+    return new Promise((resolve, reject)=>{
+        connection.query("INSERT INTO Entries SET ?",dataSet,(err,result)=>{
+            if (err){
+                return reject(err.message);
+            } else {
+                return resolve("done");
+            }
+        })
+    })
+}
 
 /* function insertEntry(entryID, time, tempBottom, tempTop, prod1Temp, prod2Temp){
     connection.query("insert into Entries (smokeID, dateTime, tempBottom, tempTop, product1Temp, product2Temp) values ("+entryID+","+timr+","+tempBottom+","+tempTop+","+prod1Temp+","+prod2Temp+")",(err,result)=>{
