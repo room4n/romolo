29bc72f9b0d249486e14069b8d720beed4324560
Jarek Romaniuk <jaroslaw.romaniuk@pl.abb.com>
2023-12-01 15:11:52 +0100

    fix

--- client/wedzarnia/src/App.js
+++ client/wedzarnia/src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div className="App">
       
       <div>
-      <Link to='/wedzarnia/'>History</Link>
-      <Link to='/wedzarnia/live'>Live</Link>
+      <Link to='/'>History</Link>
+      <Link to='/live'>Live</Link>
       <Router ></Router>
       </div>
       
