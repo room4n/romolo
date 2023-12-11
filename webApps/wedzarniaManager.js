463715a72c5b3a9c68e8c1c0943f7fe0752d004e
Jarek <room4n@gmail.com>
2023-12-11 21:41:24 +0100

    fix date

--- webApps/wedzarniaManager.js
+++ webApps/wedzarniaManager.js
@@ -16,6 +16,17 @@ wedzarniaManager.get('/', async (req,res,next)=>{
 wedzarniaManager.post('/generateData',jsonParser, async (req,res,next)=>{
     var days = req.body.days;
     var entries = req.body.entries;
+
+    var formatOptions = { 
+        day:    '2-digit', 
+        month:  '2-digit', 
+        year:   'numeric',
+        hour:   '2-digit', 
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false 
+    };
+
     var today = new Date();
     today = today.setDate(today.getDate()-5);
     var readableDate = today.toLocaleDateString('pl-PL',formatOptions).split(', ')[0];
