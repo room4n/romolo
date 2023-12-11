000191e0ce00e9fde2bdd99045f80a226ca37732
Jarek <room4n@gmail.com>
2023-12-11 21:31:34 +0100

    generator 1

--- webApps/wedzarniaManager.js
+++ webApps/wedzarniaManager.js
@@ -13,5 +13,45 @@ wedzarniaManager.get('/', async (req,res,next)=>{
     })
 });
 
+wedzarniaManager.post('/generateData',jsonParser, async (req,res,next)=>{
+    var days = req.body.days;
+    var entries = req.body.entries;
+    res.send(days);
+})
 
+wedzarniaManager.post('/deleteSmokeDay',jsonParser, async (req,res,next)=>{
+    if(Object.keys(req.body).length === 0){
+        var deleted = await removeAllSmokeDays();
+        res.send(deleted);
+    } else {
+        var deleted = await  removeSmokeDay(req.body.id);
+        res.send(deleted);
+    }
+});
+
+async function removeAllSmokeDays(){
+    return new Promise((resolve,reject)=>{
+        connection.query("DELETE from SmokeDay",(err,result)=>{
+            if(err){
+                return reject(err.message);
+            } else {
+                return resolve("All smoke days removed");
+            }
+        })
+    })
+}
+async function removeSmokeDay(id){
+    var dataSet = {
+        smokeID : id
+    }
+    return new Promise((resolve,reject)=>{
+        connection.query("DELETE from Entries where ?",dataSet,(err,result)=>{
+            if (err){
+                return reject(err.message);
+            } else {
+                return resolve("Smoke day "+id+" removed");
+            }
+        })
+    })
+}
 module.exports = wedzarniaManager;
